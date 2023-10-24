693fa7fae9d8b19aac60624e9b997ae8ecc3f5fa
samuckqadev <samuel.tomimatu@bandtec.com.br>
2023-10-24 16:59:25 -0300

    Hello World!

---
+++ src/main/java/br/com/matsutech/crudbe/CrudBeApplication.java
@@ -0,0 +1,13 @@
+package br.com.matsutech.crudbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matsutech/crudbe/CrudBeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matsutech.crudbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
